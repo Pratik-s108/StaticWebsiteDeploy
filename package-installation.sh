19be37959ef514e614dd8b5c43f4c1722a91dd73
Pratik <pratiklanjewar4198@gmail.com>
2026-01-12 19:17:04 +0530

    new file

---
+++ package-installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install unzip -y
+
+# awscli installation
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install -y
+
+# terraform installation
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform -y
+
+# docker installation 
+sudo apt install docker.io -y
+
+# kubectl installation
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+# java-17 installation
+sudo apt install openjdk-17-jdk -y
